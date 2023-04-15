bde9418ccc095738a264a3f9be20302eae78e4aa
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-04-15 12:18:34 -0500

    1 file modified

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ let name = 'Matthew';
 let age = 14;
 
 // Merging variable <- Concatenation
+
+// declaring variable
+let about = 'Hi, I am Mathew and am  years old.';
+let myAge = 14;
+
+// reassign the about variable
+about = 'Hi, I am Mathew and am ' + myAge + ' years old.';
+
+// console log the value
+console.log(about);
+
+// Console.log
+
+console.log('Hi am: ', name);
+
+// This is will leave a warning in the console
+console.warn('This is a Warning');
+
+// This is will leave a error in the console
+console.error('This is a Error');
