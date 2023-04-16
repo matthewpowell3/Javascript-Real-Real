2eb804fc7edfe8b4343f9627b697a406b7c07258
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-04-16 10:43:25 -0500

    1 file modified

--- app.js
+++ app.js
@@ -25,3 +25,57 @@ console.warn('This is a Warning');
 console.error('This is a Error');
 
 // Selecting html elements with getElementById
+
+let title = document.getElementById('title');
+
+// This will console log the HTML element
+console.log(title);
+
+// Method
+// You can you a method called innerHTML to change titles' HTML
+title.innerHTML = '<p>Hello, World</p>';
+
+// To get the text inside of the title HTML element we use innerText
+console.log(title.innerText);
+
+// Code Excersie One
+
+let aboutMe = document.getElementById('about');
+let OldText = aboutMe.innerText;
+aboutMe.innerText = 'Hello I learned Javascript';
+
+//CODE MINI PROJECT
+// 1. Create a script to concat a string and a variable together
+// 2. Using .innerHTML and getElementById change the innerHTML of an element to be what your variable is
+
+let element = document.getElementById('Info');
+let elementText = element.innerText;
+let changeTextTo = `Hello Man ${elementText}`;
+
+element.innerText = changeTextTo;
+
+console.log(element);
+
+// STRING MANIPULATION
+// e.g:
+// .toLowerCase()
+// .toUpperCase()
+// .split()
+// .join()
+// .slice()
+// .substr()
+
+let paragraphNode = document.getElementById('paragraph');
+let paragraph = paragraphNode.innerText;
+
+// this consoloe everything
+console.log(paragraphNode);
+// this only consoles the innerText
+console.log(paragraph);
+
+// .something() <- this means to do something
+let toUpperCase = paragraph.toUpperCase();
+let toLowerCase = paragraph.toLowerCase();
+let firstTenChar = paragraph.substr(10);
+
+paragraph.innerText.toUpperCase();
